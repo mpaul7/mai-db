903feccceb03ba0eee877c5041633ab8ad421e7a
mpaul7 <mpaul7@gmail.com>
2025-03-17 20:10:54 -0400

    fixed overlapping DVC tracking

--- src/retrieve_ip.py
+++ src/retrieve_ip.py
@@ -13,6 +13,19 @@ ip_not_found = defaultdict(list)
 
 from mapping_asn_v2 import *
 
+@click.command(name='current')
+def current_db():
+    """Update Current DB"""
+    with open('params.yaml', 'r') as f:
+        params = yaml.safe_load(f)
+    print(params)
+    data_source_dir = params['current_db']['data_source_dir']
+    os.makedirs(params['current_db']['data_dir'], exist_ok=True)
+    file_name = params['current_db']['file_name']
+    file_path = os.path.join(data_source_dir, file_name)
+    cp = subprocess.run(f"cp {file_path} {params['current_db']['data_dir']}", shell=True)
+    cp.check_returncode()   
+    
 def _run_whois_impl(iterations=1):
     """ Implementation of whois command to retrieve route information """
     def extract_route_info(whois_output):
@@ -58,13 +71,11 @@ def _run_whois_impl(iterations=1):
     df.drop_duplicates(subset=['net_address'], keep='first', inplace=True)
     return df, ip_not_found
 
-
 @click.command(name='asn')
 def run_whois():
     """ Run whois command to retrieve route information """
     with open('params.yaml', 'r') as f:
         params = yaml.safe_load(f)
-    print(params)
     iterations = params['retrieve_db']['iterations']
 
     df, ip_not_found = _run_whois_impl(iterations=iterations)
@@ -89,7 +100,6 @@ def update_current_db():
     """Update Current DB"""
     with open('params.yaml', 'r') as f:
         params = yaml.safe_load(f)
-    print(params)
     
     """Current DB
      - Read current DB
@@ -123,23 +133,11 @@ def update_current_db():
     updated_df.drop_duplicates(subset=['net_address'], keep='first', inplace=True)
     updated_df.to_csv(updated_file, index=False)
     
-    """Final DB
-     - Remove duplicates from updated DB
-     - Save to file as final DB
-     - Generate DB comparison report
-    """
-    final_data_dir = params['final_db']['data_dir']
-    os.makedirs(final_data_dir, exist_ok=True)
-    final_file_name = params['final_db']['file_name']
-    final_file = os.path.join(final_data_dir, final_file_name)
-    final_df = updated_df[['net_address', 'label']].copy()
-    final_df.to_csv(final_file, index=False)
-  
     """Compare record counts by label between current and retrieved DBs
-    - Generate comparison report
-    - Display comparison report
-    - Save comparison report to file in updated_data_dir
-    """
+        - Generate comparison report
+        - Display comparison report
+        - Save comparison report to file in updated_data_dir
+        """
     current_by_label = current_df.groupby('label').size()
     retrieved_by_label = retrieve_df.groupby('label').size()
     
@@ -162,12 +160,65 @@ def update_current_db():
     click.echo(f"Comparison Report\n")
     click.echo(comparison_df)
     click.echo(f"\n{'='*50}\n")
+
+    
+@click.command(name='final')
+def final_db():
+    """Final DB
+        - Remove duplicates from updated DB
+        - Save to file as final DB
+    - Generate DB comparison report
+"""
+    """Update final DB"""
+    with open('params.yaml', 'r') as f:
+        params = yaml.safe_load(f)
+        
+    final_data_dir = params['final_db']['data_dir']
+    os.makedirs(final_data_dir, exist_ok=True)
+    final_file_name = params['final_db']['file_name']
+    final_file = os.path.join(final_data_dir, final_file_name)
+    
+    updated_data_dir = params['updated_db']['data_dir']
+    updated_file_name = params['updated_db']['file_name']
+    updated_file = os.path.join(updated_data_dir, updated_file_name)
+    updated_df = pd.read_csv(updated_file)
+    final_df = updated_df[['net_address', 'label']].copy()
+    final_df.to_csv(final_file, index=False)
+
+    # """Compare record counts by label between current and retrieved DBs
+    # - Generate comparison report
+    # - Display comparison report
+    # - Save comparison report to file in updated_data_dir
+    # """
+    # current_by_label = current_df.groupby('label').size()
+    # retrieved_by_label = retrieve_df.groupby('label').size()
+    
+    # current_by_label = current_df.groupby('label').size().rename("current_IP_count")
+    # retrieved_by_label = retrieve_df.groupby('label').size().rename("retrieved_IP_count")
+
+    # # Merge both series into a single DataFrame for comparison
+    # comparison_df = pd.concat([current_by_label, retrieved_by_label], axis=1).fillna(0)
+
+    # # Convert NaN to 0 and ensure integer type
+    # comparison_df = comparison_df.astype(int)
+
+    # # Calculate the difference
+    # comparison_df["difference"] = comparison_df["current_IP_count"] - comparison_df["retrieved_IP_count"]
+    # comparison_df.sort_values(by='difference', ascending=False, inplace=True)
+    # comparison_df.to_csv(os.path.join(updated_data_dir, f"comparison_current_retrieved_db.csv"))
+
+    # # Display the comparison
+    # click.echo(f"\n{'='*50}\n")
+    # click.echo(f"Comparison Report\n")
+    # click.echo(comparison_df)
+    # click.echo(f"\n{'='*50}\n")
     
 
 @click.group()
 def entrypoint():
     """ Based on ASN, generate a CSV file containing 'net_address' and 'label' """
 
-
+entrypoint.add_command(current_db)
 entrypoint.add_command(run_whois)
 entrypoint.add_command(update_current_db)
+entrypoint.add_command(final_db)
